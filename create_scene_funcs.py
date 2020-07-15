6fc501e2304b4e554cd7a349c48d472caa124a2e
Len Wanger <len_wanger@hotmail.com>
2020-07-14 21:12:29 -0500

    Added create_perlin_1

--- create_scene_funcs.py
+++ create_scene_funcs.py
@@ -24,7 +24,7 @@ from texture_classes import SolidColor, CheckerBoard, ImageTexture, NoiseTexture
 from primitives_classes import Sphere, Plane, Triangle, Disc, STLMesh
 from light_classes import PointLight, AreaLight
 from scene import Scene
-from perlin import value_noise, turbulent_noise, fractal_noise
+from perlin import value_noise, turbulent_noise, fractal_noise, wood_pattern, marble_pattern
 
 
 def create_simple_world(settings=None):
@@ -506,95 +506,128 @@ def create_canonical_2(settings=None):
 
 def create_perlin_1(settings=None):
     # sphere over a plane!
-    silver = SolidColor(Vec3(0.7, 0.7, 0.7))
+    # dragon over a plane!
 
-    palette = cc.fire
-    palette2 = cc.kgy
-    colormap = [get_color(i, palette) for i in range(len(palette))]
-    # colormap2 = [get_color(i, palette2) for i in range(len(palette2))]
-    colormap2 = [get_color(i, palette2) for i in range(len(palette2)//2)]
+    green = SolidColor(Vec3(0.2,0.7,0.2))
+    brown = SolidColor(Vec3(0.7,0.5,0.3))
 
-    # noise2 = NoiseTexture(colormap2, point_scale=5.0, name='jade')
-    # noise2 = NoiseTexture(colormap, point_scale=5.0, eval_func=value_noise, name='jade')
+    # point_scale = 1.0
+    # wood_point_scale = 100.0
+    wood_point_scale = 20.0
 
-    frequency = 0.02
-    frequency_mult = 1.8
-    amplitude_mult = 0.35
-    num_layers = 5
-    # sub_val = -0.559
-    sub_val = 0.0
-    # div_val = .5227 - sub_val
-    div_val = .5227
+    wood, wood_name = (cc.CET_D6[155:240], 'wood3')
+    wood_colormap = [get_color(i, wood) for i in range(len(wood))]
+    kwargs = {'frequency': 0.01, 'frequency_mult': 10, }
+    translate = 1.0
+    scale = 0.5
+    wood_texture = NoiseTexture(wood_colormap, point_scale=wood_point_scale, translate=translate, scale=scale,
+                              name=wood_name, eval_func=wood_pattern, eval_kwargs=kwargs)
 
-    kwargs={ 'frequency': frequency, 'frequency_mult': frequency_mult, 'amplitude_mult': amplitude_mult,
-             'layers': num_layers, 'sub_val': sub_val, 'div_val': div_val}
+    jade, jade_name = (cc.CET_D13[135:240], 'jade2')
+    jade_colormap = [get_color(i, jade) for i in range(len(jade))]
+    kwargs = {'frequency': 0.024, 'frequency_mult': 2.5, 'amplitude_mult': 0.5, 'layers': 7, 'displace_x': 200}
+    translate = 0.20
+    scale = 1.0
+    # jade_point_scale = 600.0
+    jade_point_scale = 6.0
+    jade_texture = NoiseTexture(jade_colormap, point_scale=jade_point_scale, translate=translate, scale=scale,
+                              name=jade_name, eval_func=marble_pattern, eval_kwargs=kwargs)
 
-    # sub_val = 0.458
-    sub_val = 0.0
-    # div_val = 2.742 - sub_val
-    div_val = 2.742
+    # diffuse_1 = Lambertian(wood_texture, name="wood'")
+    diffuse_2 = Lambertian(jade_texture, name="jade")
+    diffuse_3 = Lambertian(green, name="solid green")
+    diffuse_4 = Lambertian(brown, name="solid brown")
 
-    kwargs2 = {'frequency': frequency, 'frequency_mult': frequency_mult, 'amplitude_mult': amplitude_mult,
-              'layers': num_layers, 'sub_val': sub_val, 'div_val': div_val}
+    metal_1 = Metal(wood_texture, name="shiny wood", fuzziness=0.2)
+    metal_2 = Metal(jade_texture, name="metal_1", fuzziness=0.3)
 
-    noise = NoiseTexture(colormap, point_scale=2.0, name='fire', eval_func=fractal_noise, eval_kwargs=kwargs)
+    ground_matl = metal_1
+    # ground_matl = diffuse_4
 
-    noise2 = NoiseTexture(colormap2, point_scale=10.0, eval_func=turbulent_noise, eval_kwargs=kwargs2, name='jade')
-
-    odd_color = SolidColor(Vec3(0.2, 0.75, 0.2))
-    even_color = SolidColor(Vec3(0.1, 0.1, 0.1))
-    checker_board = CheckerBoard(even_color, odd_color, spacing=2.0)
-
-    # diffuse_1 = Lambertian(checker_board, name="noise'")
-    diffuse_1 = Lambertian(noise, name="noise'")
-    diffuse_2 = Lambertian(noise2, name="noise2")
-    # diffuse_2 = Lambertian(checker_board, name="checkerboard")
-    metal_1 = Metal(silver, name="metal_1")
-    # metal_1 = Metal(noise2, name="metal_1")
+    object_matl = diffuse_2
+    # object_matl = metal_2
+    # object_matl = diffuse_3
 
     world = GeometryList()
 
-    world.add(Sphere(Vec3(0, 1.25, 0.35), 1.0, diffuse_2))
-    # world.add(Sphere(Vec3(0, 1.25, 0.35), 1.0, metal_1))
-
-    # if True:  # use plane vs triangles
     if False:
-        plane_1 = Plane.plane_from_point_and_normal(pt=Vec3(0, -1, 0), normal=Vec3(0, 1, 0), material=diffuse_1)
-        world.add(plane_1)
+    # if True:
+        world.add(Sphere(Vec3(0, 0.0, 0.0), 8.0, object_matl))
+        settings = {'look_from': Vec3(0.0, 10, 40), 'look_at': Vec3(0, 0.25, 0),
+                    'plane_x': 24, 'plane_y': -8.0, 'back_plane_z': -25, 'front_plane_z': 20,
+                    'rot_axis': [1, 0, 0], 'rot_rads': math.pi / 2, 'translate': [0, 0, -12.5], 'show_walls': True}
     else:
-        plane_x = 2
-        plane_y = -1
-        back_plane_z = -2
-        front_plane_z = 3
-        v0 = Vec3(-plane_x, plane_y, front_plane_z)
-        uv0 = (0,0)
-        v1 = Vec3(-plane_x ,plane_y,back_plane_z)
-        uv1 = (0, 1)
-        v2 = Vec3(plane_x, plane_y, front_plane_z)
-        uv2 = (1, 0)
-        v3 = Vec3(plane_x, plane_y,back_plane_z)
-        uv3 = (1, 1)
-        triangle = Triangle(v0, v1, v2, diffuse_1, uv0, uv1, uv2)
-        world.add(triangle)
+        # Stanford dragon - bbox=(vmin=(-10.832, -4.205, 5.278), vmax=(9.657, 4.973, 19.742)), num_triangles=304941
+        # rotated by 90 deg on the X axis... bbox=AABB(vmin=(-48.551, 5.275, -45.792), vmax=(59.196, 113.167, 42.010)), num_triangles=112402
+        stl_filename = "models/dragon_65.stl"
+        # settings = {'look_from': Vec3(0.0, 10, 35), 'look_at': Vec3(0, 0.25, 0),
+        settings = {'look_from': Vec3(0.0, 10, 40), 'look_at': Vec3(0, 0.25, 0),
+        # 'plane_x': 24, 'plane_y': -4.205, 'back_plane_z': -25, 'front_plane_z': 20,
+        'plane_x': 24, 'plane_y': -7.221, 'back_plane_z': -25, 'front_plane_z': 20,
+        # 'rot_axis': [1,0,0], 'rot_rads': math.pi/2, 'translate': [-25, 0, 0], 'show_walls': True}
+        'rot_axis': [1,0,0], 'rot_rads': math.pi/2, 'translate': [0, 0, -12.5], 'show_walls': True}
+        # 'rot_axis': [0,1,0], 'rot_rads': math.pi, 'translate': [0, 0, -12.5], 'show_walls': False }
 
-        triangle = Triangle(v1, v2, v3, diffuse_1, uv1, uv2, uv3)
-        world.add(triangle)
+        # if False:
+        if True:
+            my_mesh = mesh.Mesh.from_file(stl_filename)
 
-    ambient = Vec3(0.5,0.5,0.5)
+            if 'translate' in settings:
+                settings['translate'][0]
+                my_mesh.translate([settings['translate'][0], settings['translate'][1], settings['translate'][2]])
+
+            if 'rot_axis' in settings and settings['rot_axis'] is not None:
+                rot_axis = settings['rot_axis']
+                rot_rads = settings['rot_rads']
+                my_mesh.rotate(rot_axis, rot_rads)
+
+            stl_mesh = STLMesh(my_mesh, object_matl, name="mesh_1")
+            print(f'stl_mesh {stl_filename} -- bbox={stl_mesh.bounding_box(None, None)}, num_triangles={stl_mesh.num_triangles}')
+            world.add(stl_mesh)
+
+    if True:
+        # if True:  # use plane vs triangles
+        if False:
+            plane_1 = Plane.plane_from_point_and_normal(pt=Vec3(0, -1, 0), normal=Vec3(0, 1, 0), material=ground_matl)
+            world.add(plane_1)
+        else:
+            plane_x = settings['plane_x']
+            plane_y = settings['plane_y']
+            back_plane_z = settings['back_plane_z']
+            front_plane_z = settings['front_plane_z']
+            v0 = Vec3(-plane_x, plane_y, front_plane_z)
+            uv0 = (0,0)
+            v1 = Vec3(-plane_x ,plane_y,back_plane_z)
+            uv1 = (0, 1)
+            v2 = Vec3(plane_x, plane_y, front_plane_z)
+            uv2 = (1, 0)
+            v3 = Vec3(plane_x, plane_y,back_plane_z)
+            uv3 = (1, 1)
+            triangle = Triangle(v0, v1, v2, ground_matl, uv0, uv1, uv2)
+            world.add(triangle)
+
+            triangle = Triangle(v1, v2, v3, ground_matl, uv1, uv2, uv3)
+            world.add(triangle)
+
+    ambient = Vec3(0.6,0.6,0.6)
     background = SolidColor(Vec3(0.5, 0.7, 1.0))
 
-    geom = Disc(center=Vec3(11,10,3), normal=Vec3(0,-1,0), radius=1.5, material=SolidColor(Vec3(0.7, 0.7, 0.7)))
+    geom = Disc(center=Vec3(3,10,-3), normal=Vec3(0,-1,0), radius=1.5, material=SolidColor(Vec3(0.7, 0.7, 0.7)))
     if settings and 'SAMPLES_PER_LIGHT' in settings:
         samples = settings['SAMPLES_PER_LIGHT']
     else:
         samples = 25
 
-        # light_1 = PointLight(pos=Vec3(-10.0, 100, 80), color=Vec3(0.2, 0.2, 0.2))
+        # light_1 = PointLight(pos=Vec3(-10.0, 100, 80), color=Vec3(0.2, 0.3, 0.2))
+        light_1 = PointLight(pos=Vec3(-10.0, 100, 80), color=Vec3(0.6, 0.6, 0.6))
         light_2 = AreaLight(geom=geom, color=Vec3(0.6, 0.6, 0.6), num_samples=samples)
 
+    # lights = [light_1]
     lights = [light_2]
+    # lights = [light_1, light_2]
     scene = Scene(world, ambient=ambient, lights=lights, background=background)
-    camera = Camera(look_from=Vec3(8.5, 4, 0), look_at=Vec3(0, 1, 0), vup=Vec3(0, 1, 0), vert_fov=25)
+    # camera = Camera(look_from=Vec3(8.5, 4, 0), look_at=Vec3(0, 1, 0), vup=Vec3(0, 1, 0), vert_fov=25)
+    camera = Camera(look_from=settings['look_from'], look_at=settings['look_at'], vup=Vec3(0, 1, 0), vert_fov=25)
 
     return {'scene': scene, 'camera': camera}
 
